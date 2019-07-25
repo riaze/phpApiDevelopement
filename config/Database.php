58ee0d212414fdb913ee85cb87cfa838f4376f68
Riaze ahamed <38366023+riaze@users.noreply.github.com>
2019-07-25 20:04:44 +0200

    changé de nom de base de données

--- config/Database.php
+++ config/Database.php
@@ -2,7 +2,7 @@
 
 class Database {
     private $host = 'localhost';
-    private $db_name = 'myblog';
+    private $db_name = 'monblog';
     private $username = 'root';
     private $password = 'riaze';
     private $conn;
